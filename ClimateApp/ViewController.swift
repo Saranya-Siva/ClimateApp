971a7120d90ffe53dd7980e690d19c8591bc827a
Saranya-Siva <saran.ch41@gmail.com>
2020-06-30 01:02:55 -0500

    Initial Commit

---
+++ ClimateApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClimateApp
+//
+//  Created by Saranya Kalyanasundaram on 6/30/20.
+//  Copyright © 2020 Saranya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
